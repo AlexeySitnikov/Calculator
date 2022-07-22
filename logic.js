b7c698c518b27592ac587f2a49092f41dd0af1cb
AlexeySitnikov <alexey.sitnikov@bk.ru>
2022-07-22 15:13:17 +0300

    Almost fixed logic

--- logic.js
+++ logic.js
@@ -1,64 +1,16 @@
 let fisrtOperand = 0;
 let secondOperand = 0;
-let addFlag = false;
-let divideFlag = false;
-let multiplyFlag = false;
-let minusFlag = false;
-let abc = "";
+let firstOperation = "";
+let abc = "0";
 let wasDot = false;
 
-function buttonOperation(operation) {
-  fisrtOperand = Number(document.getElementById("outputLine").value);
-  abc = "";
-  wasDot = false;
-  if (operation === "+") {
-    addFlag = true;
-  } else if (operation === "-") {
-    minusFlag = true;
-  } else if (operation === "*") {
-    multiplyFlag = true;
-  } else if (operation === "/") {
-    divideFlag = true;
-  }
-}
-
-function buttonErase() {
+function buttonErase() {  
+  document.getElementById("outputLine").value = "0";
   fisrtOperand = 0;
   secondOperand = 0;
-  addFlag = false;
-  divideFlag = false;
-  multiplyFlag = false;
-  minusFlag = false;
-  document.getElementById("outputLine").value = "0";
-  abc = "";
+  firstOperation = "";
+  abc = "0";
   wasDot = false;
-}
-
-function buttonEqual() {
-  secondOperand = Number(abc);
-  let result = Number(0);
-  if (addFlag === true) {
-    addFlag = false;
-    result = fisrtOperand + secondOperand;
-  } else if (minusFlag === true) {
-    minusFlag = false;
-    result = fisrtOperand - secondOperand;
-  } else if (multiplyFlag === true) {
-    multiplyFlag = false;
-    result = fisrtOperand * secondOperand;
-  } else if (divideFlag === true) {
-    if (secondOperand !== 0){
-      divideFlag = false;
-      result = fisrtOperand / secondOperand;
-    } else {
-      alert("Division by zero!");
-      buttonErase();
-      return;      
-    }
-    
-  }
-  document.getElementById("outputLine").value = result;
-  fisrtOperand = result;
 }
 
 document.querySelector(".buttons").addEventListener("click", handleClick);
@@ -69,11 +21,51 @@ function handleClick(event) {
   }
 
   let buttonValue = event.target.innerText;
-  if (buttonValue === "." && wasDot !== true) {
-    abc += ".";
+
+  operand = getValue(buttonValue);
+  operation = getOperation(buttonValue);
+
+  if (operand !== false) {
+    if (abc === "0") {
+      if (operand === ".") {
+        abc += operand;
+      } else {
+        abc = operand;
+      }
+    } else {
+      abc += operand;
+    }
     document.getElementById("outputLine").value = abc;
-    wasDot = true;
-  } else if (
+  } else if (operation !== false) {
+    if (firstOperation === "") {
+      firstOperation = operation;
+      fisrtOperand = abc;
+      wasDot = false;
+      abc = "0";
+    } else if (operation === "=") {
+      secondOperand = abc;
+      result = calculateValue();
+      document.getElementById("outputLine").value = result;
+      fisrtOperand = result;
+      wasDot = false;
+      abc = result;
+      firstOperation = "";
+    } else {
+      secondOperand = abc;
+      result = calculateValue();
+      document.getElementById("outputLine").value = result;
+      fisrtOperand = result;
+      wasDot = false;
+      abc = "0";
+      firstOperation = operation;
+    }
+  } else if (buttonValue === "Erase"){
+    buttonErase();
+  }
+}
+
+function getValue(buttonValue) {
+  if (
     buttonValue === "1" ||
     buttonValue === "2" ||
     buttonValue === "3" ||
@@ -85,18 +77,41 @@ function handleClick(event) {
     buttonValue === "9" ||
     buttonValue === "0"
   ) {
-    abc += buttonValue;
-    document.getElementById("outputLine").value = abc;
-  } else if (
+    return buttonValue;
+  } else if (buttonValue === "." && wasDot !== true) {
+    wasDot = true;
+    return buttonValue;
+  } else return false;
+}
+
+function getOperation(buttonValue) {
+  if (
     buttonValue === "+" ||
     buttonValue === "-" ||
     buttonValue === "*" ||
-    buttonValue === "/"
+    buttonValue === "/" ||
+    buttonValue === "="
   ) {
-    buttonOperation(buttonValue);
-  } else if (buttonValue === "=") {
-    buttonEqual();
-  } else if (buttonValue === "Erase") {
-    buttonErase();
+    return buttonValue;
+  } else return false;
+}
+
+function calculateValue() {
+  let result = Number(0);
+  if (firstOperation === "+") {
+    result = Number(fisrtOperand) + Number(secondOperand);
+  } else if (firstOperation === "-") {
+    result = Number(fisrtOperand) - Number(secondOperand);
+  } else if (firstOperation === "*") {
+    result = Number(fisrtOperand) * Number(secondOperand);
+  } else if (firstOperation === "/") {
+    if (secondOperand !== 0) {
+      result = Number(fisrtOperand) / Number(secondOperand);
+    } else {
+      alert("Division by zero!");
+      buttonErase();
+      return;
+    }
   }
+  return result;
 }
